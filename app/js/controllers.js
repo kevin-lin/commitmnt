320ea10e310d62d0fa53c49af07724dbdee16633
Kevin Lin <whatakevin@gmail.com>
2014-07-02 00:10:09 -0500

    Integrate firebase and angularFire

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,12 +2,13 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
+angular.module('myApp.controllers', ["firebase"])
   .controller('HomeCtrl', ['$scope', function($scope) {
 
   }])
-  .controller('MyCtrl1', ['$scope', function($scope) {
-
+  .controller('MyCtrl1', ['$scope', '$firebase', function($scope, $firebase) {
+    var commitmntRef = new Firebase("https://commitmnt.firebaseio.com/commitmnts");
+    $scope.commitmnts = $firebase(commitmntRef);
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
 
