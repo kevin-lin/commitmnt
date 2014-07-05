7510837c401c49c0c56052c47582c17e173021aa
Kevin Lin <whatakevin@gmail.com>
2014-07-04 23:27:23 -0500

    Add basic log in functionality

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,9 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'});
+  $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'MainCtrl'});
+  $routeProvider.when('/new_user', {templateUrl: 'partials/new_user.html', controller: 'NewUserCtrl'});
+  $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/home'});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,29 @@
 /* Controllers */
 
 angular.module('myApp.controllers', ["firebase"])
-  .controller('HomeCtrl', ['$scope', function($scope) {
-
-  }])
+  .controller('MainCtrl', ['$scope', '$firebase', '$firebaseSimpleLogin',
+    function($scope, $firebase, $firebaseSimpleLogin) {
+      var ref = new Firebase('https://commitmnt.firebaseio.com/');
+      $scope.auth = $firebaseSimpleLogin(ref);
+    }
+  ])
+  .controller('NewUserCtrl', ['$scope',
+    function($scope) {
+      $scope.createUser = function(){
+        $scope.auth.$createUser($scope.username, $scope.password);
+      };
+    }
+  ])
+  .controller('LoginCtrl', ['$scope',
+    function($scope) {
+      $scope.login = function(){
+        $scope.auth.$login('password', {
+          email: $scope.username,
+          password: $scope.password
+        });
+      };
+    }
+  ])
   .controller('MyCtrl1', ['$scope', '$firebase', function($scope, $firebase) {
     var commitmntRef = new Firebase("https://commitmnt.firebaseio.com/commitmnts");
     $scope.commitmnts = $firebase(commitmntRef);
